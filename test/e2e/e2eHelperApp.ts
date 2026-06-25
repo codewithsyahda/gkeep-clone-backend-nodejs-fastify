f100b36d389c98a90ff6ffd8029d95a7a676f135
Syahda Romansyah <codewithsyahda@gmail.com>
2026-06-25 15:05:21 +0700

    chore: remove unused code on the e2eHelperApp
    Remove unused Better Auth on the e2eHelperApp.

--- test/e2e/e2eHelperApp.ts
+++ test/e2e/e2eHelperApp.ts
@@ -1,9 +1,7 @@
 import '../config/dotenv.js';
 
-import { fromNodeHeaders } from 'better-auth/node';
 import Fastify from 'fastify';
 
-import auth from './common/lib/auth.js';
 import prismaClient from './common/lib/prismaClient.js';
 
 const fastify = Fastify({
@@ -20,38 +18,6 @@ const fastify = Fastify({
 
 fastify.register(
   async (fst) => {
-    fst.route({
-      method: ['GET', 'POST'],
-      url: '/auth/*',
-      async handler(request, reply) {
-        try {
-          // Construct request URL
-          const url = new URL(request.url, `http://${request.headers.host}`);
-
-          // Convert Fastify headers to standard Headers object
-          const headers = fromNodeHeaders(request.headers);
-          // Create Fetch API-compatible request
-          const req = new Request(url.toString(), {
-            method: request.method,
-            headers,
-            ...(request.body ? { body: JSON.stringify(request.body) } : {}),
-          });
-          // Process authentication request
-          const response = await auth.handler(req);
-          // Forward response to client
-          reply.status(response.status);
-          response.headers.forEach((value, key) => reply.header(key, value));
-          return reply.send(response.body ? await response.text() : null);
-        } catch (error) {
-          fastify.log.error(error, 'Authentication Error');
-          return reply.status(500).send({
-            error: 'Internal authentication error',
-            code: 'AUTH_FAILURE',
-          });
-        }
-      },
-    });
-
     fst.get('/health/liveness', async (_request, replay) => {
       await replay.status(200).send({
         data: {
